41efd9cd481877fd74fabe2c4e7878acd5cf86cf
iampriyabrat14 <datasc521@gmail.com>
2025-05-31 21:27:54 +0530

    smal small changes replated to file done

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,7 @@
+from langchain_astradb import AstraDBVectorStore
+from dotenv import load_dotenv
+import pandas as pd
+import os
+from data_ingestion.data_transformation import Data_converter
+
+
---
+++ data_ingestion/data_transformation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+
+class Data_converter():
+    def __init__(self):
+        pass
+
+
+    def data_transformation(self):
+        pass
+
+
